4c0f5bbaea45fa5d4b463874a3e84f9fc3a34497
saifee56 <safuanalam123@gmail.com>
2025-10-27 22:07:08 +0600

    authentication test case updated

--- tests/test_authentication/test_authentication_viewset.py
+++ tests/test_authentication/test_authentication_viewset.py
@@ -3,6 +3,16 @@ import pytest
 from rest_framework.test import APIClient
 from authentication.models import CustomUserModel
 
+@pytest.fixture
+def create_test_user():
+    user=CustomUserModel.objects.create_user(
+        username="saifee",
+        password="saifee123",
+        email="safuan@gmail.com",
+        role="user"
+    )
+    return user
+
 @pytest.mark.django_db
 class TestAuthenticationViewset:
 
@@ -40,3 +50,39 @@ class TestAuthenticationViewset:
         assert response.data["success"] is False
         assert response.data["message"]=="Signup failed"
         assert "errors" in response.data
+    
+    def test_login_success(self,create_test_user):
+        data={
+            "email":create_test_user.email,
+            "password":"saifee123"
+        }
+
+        response=self.client.post(self.login_url,data,format="json")
+        assert response.status_code == 200
+        assert response.data["success"]==True
+        assert "access" in response.data["data"]
+    
+    def test_login_invalid_credentials(self,create_test_user):
+
+        data1={
+            "email":create_test_user.email,
+            "password":"saifee12"
+        }
+        data2={
+            "email":"safuan12345@gmail.com",
+            "password":"saifee123"
+        }
+
+    @pytest.mark.parametrize("email,password,expected_status",[
+        ("safuan@gmail.com","saifee12",401),
+        ("safuan12345@gmail.com","saifee123",401),
+    ])
+    def test_login_invalid_credentials(self,create_test_user,email,password,expected_status):
+        data={
+            "email":email,
+            "password":password
+        }
+
+        response=self.client.post(self.login_url,data,format="json")
+        assert response.status_code==expected_status
+        assert response.data["success"] is False
